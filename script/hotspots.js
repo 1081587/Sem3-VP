08767de251f184d8652d6fb09c3898b4c5bd7cc4
radinamar00 <radinaamar2000@gmail.com>
2020-09-27 13:01:29 +0200

    last changes

--- script/hotspots.js
+++ script/hotspots.js
@@ -1,14 +1,13 @@
-
 let video2played = false;
 let video3played = false;
 let video4played = false;
 
-const fps = 30; 
-const debug = false; 
+const fps = 30;
+const debug = false;
 
 
-const msInterval = Math.floor(1000 / fps); 
-let engine; 
+const msInterval = Math.floor(1000 / fps);
+let engine;
 
 let video = {
     log: (message = 'Missing log text') => {
@@ -20,50 +19,50 @@ let video = {
         running: false,
         init: () => {
             video.log('video hotspot engine: init');
-            const elmsVideo = document.querySelectorAll('video'); 
+            const elmsVideo = document.querySelectorAll('video');
             elmsVideo.forEach((elmVideo) => {
                 elmVideo.parentElement.classList.add('videoHotspotsParent');
-                elmVideo.addEventListener('play', (event) => { 
-                    if (!video.hotspots.running) { 
+                elmVideo.addEventListener('play', (event) => {
+                    if (!video.hotspots.running) {
                         video.hotspots.on();
                     }
                 });
-                elmVideo.addEventListener('seeked', (event) => { 
-                    if (!video.hotspots.running) { 
+                elmVideo.addEventListener('seeked', (event) => {
+                    if (!video.hotspots.running) {
                         video.hotspots.on(true);
                     }
                 });
-                elmVideo.addEventListener('pause', (event) => { 
-                    if (video.hotspots.running) { 
-                        let videoPlaying = false; 
+                elmVideo.addEventListener('pause', (event) => {
+                    if (video.hotspots.running) {
+                        let videoPlaying = false;
                         elmsVideo.forEach((elmVideo) => {
                             if (!elmVideo.paused) {
                                 videoPlaying = true;
                             }
                         });
                         if (!videoPlaying) {
-                            video.hotspots.off(); 
+                            video.hotspots.off();
                         }
                     }
                 });
             });
         },
         on: (isSeeked = false) => {
-           
+
             video.log('video hotspot engine: on');
-            if (!video.hotspots.running) { 
+            if (!video.hotspots.running) {
                 video.hotspots.running = true;
             }
-            engine = setInterval(() => { 
+            engine = setInterval(() => {
                 video.log('engine loop');
                 video.hotspots.update(isSeeked);
             }, msInterval);
         },
         off: () => {
-            
+
             video.log('video hotspot engine: off');
-            video.hotspots.running = false; 
-            clearInterval(engine); 
+            video.hotspots.running = false;
+            clearInterval(engine);
         },
         update: () => {
 
@@ -78,10 +77,10 @@ let video = {
                             if (elmHotspotCheck) {
                                 const elmHotspot = document.querySelector(`#hotspotId${index}`);
                                 elmHotspot.parentElement.removeChild(elmHotspot);
-                                hotspot.onscreen = false; 
+                                hotspot.onscreen = false;
                             }
                         } else if (hotspot.markIn <= now && hotspot.markOut > now) {
-                            if (!elmHotspotCheck) { 
+                            if (!elmHotspotCheck) {
                                 let elmHotspot = document.createElement('a');
                                 elmHotspot.id = `hotspotId${index}`;
                                 elmHotspot.className = 'hotspot';
@@ -98,25 +97,25 @@ let video = {
                                 css += `top: ${hotspot.posY}%;`;
                                 css += `${hotspot.ui.style};`;
                                 if (hotspot.ui.type == 'image') {
-                                    
+
                                     css += `background-image: url(${hotspot.ui.image});`;
                                     elmHotspot.classList.add('image');
                                 }
                                 elmHotspot.style = css;
                                 if (hotspot.hotspot.type == 'link') {
-                                 
+
                                     elmHotspot.href = hotspot.hotspot.url;
                                     elmHotspot.target = hotspot.hotspot.target;
                                 } else {
-                                   
+
                                     elmHotspot.addEventListener('click', (event) => {
-                                        event.preventDefault(); 
-                                        hotspot.hotspot.func(); 
+                                        event.preventDefault();
+                                        hotspot.hotspot.func();
                                     });
                                 }
                                 video.parentElement.appendChild(elmHotspot);
                                 video.pause();
-                               
+
                                 if (hotspot.ui.pause) {
                                     videojs(hotspot.videoId).pause();
                                 }
@@ -127,15 +126,15 @@ let video = {
             });
         },
         remove: () => {
-        
+
             video.log('video hotspot engine: cleanup');
-            video.hotspots.off(); 
-            const elmsHotspots = document.querySelectorAll('a.hotspot'); 
-            elmsHotspots.forEach((elmHotspot) => { 
-                elmHotspot.parentElement.removeChild(elmHotspot); 
+            video.hotspots.off();
+            const elmsHotspots = document.querySelectorAll('a.hotspot');
+            elmsHotspots.forEach((elmHotspot) => {
+                elmHotspot.parentElement.removeChild(elmHotspot);
             });
-            delete video; 
-            delete hotspots; 
+            delete video;
+            delete hotspots;
         }
 
     }
@@ -157,8 +156,8 @@ const vid4 = document.querySelector('#video4');
 const hotspots = [{
         active: true,
         videoId: "video1",
-        markIn: 43,
-        markOut: 44,
+        markIn: 39,
+        markOut: 40,
         sizeX: 15,
         sizeY: 15,
         posX: 14,
@@ -181,8 +180,8 @@ const hotspots = [{
     {
         active: true,
         videoId: "video1",
-        markIn: 43,
-        markOut: 44,
+        markIn: 39,
+        markOut: 40,
         sizeX: 15,
         sizeY: 15,
         posX: 60,
@@ -206,8 +205,8 @@ const hotspots = [{
     {
         active: true,
         videoId: "video1",
-        markIn: 43,
-        markOut: 44,
+        markIn: 39,
+        markOut: 40,
         sizeX: 15,
         sizeY: 15,
         posX: 78,
@@ -227,20 +226,66 @@ const hotspots = [{
             }
         }
     },
-    
+
     {
         active: true,
         videoId: "video1",
-        markIn: 43,
-        markOut: 47,
+        markIn: 39,
+        markOut: 40,
         sizeX: 70,
         sizeY: 40,
-        posX: 20,
-        posY: 70,
+        posX: 21,
+        posY: 80,
         ui: {
             type: "box",
             text: "Click one of the icons above.<br> Watch each students unique story!",
-            style: "font-size:3.5vw; font-weight: 200; height: auto; width: auto; text-align: center; background-color: #F85F02;"
+            style: "font-size:2.6vw; height: auto; padding: 1% 2%; width: auto; text-align: center; color: #F85F02; background-color: white; font-family: 'Montserrat', sans-serif; border-radius: 500px; line-height: 1.1;"
+        },
+        hotspot: {
+            type: "function",
+            func: () => {
+                console.log("Internal screaming!");
+            }
+        }
+    },
+
+
+    {
+        active: true,
+        videoId: "video1",
+        markIn: 71,
+        markOut: 72,
+        sizeX: 70,
+        sizeY: 40,
+        posX: 36,
+        posY: 40,
+        ui: {
+            type: "box",
+            text: "Interested?",
+            style: "font-size:3.5vw; height: auto; padding: 1% 2%; width: auto; text-align: center; color: #F85F02;  font-family: 'Montserrat', sans-serif; border-radius: 500px; line-height: 1.4;"
+        },
+        hotspot: {
+            type: "function",
+            func: () => {
+                console.log("Internal screaming!");
+            }
+        }
+    },
+
+
+    {
+        active: true,
+        videoId: "video1",
+        markIn: 71,
+        markOut: 72,
+        sizeX: 70,
+        sizeY: 40,
+        posX: 19,
+        posY: 50,
+        ui: {
+            type: "box",
+            text: "Scroll down to see our special offer",
+            style: "font-size:3.3vw; height: auto; padding: 1% 2%; width: auto; text-align: center; color: white;  font-family: 'Montserrat', sans-serif; border-radius: 500px; line-height: 1.4;"
         },
         hotspot: {
             type: "function",
@@ -250,6 +295,8 @@ const hotspots = [{
         }
     }
 
+
+
 ];
 
 videojs('video2').on('ended', () => {
@@ -257,10 +304,10 @@ videojs('video2').on('ended', () => {
     div1.classList.remove("hidden");
     video2played = true
     if (video2played && video3played && video4played) {
-        videojs('video1').currentTime(45);
+        videojs('video1').currentTime(42);
         videojs('video1').play();
     } else {
-        videojs('video1').currentTime(43);
+        videojs('video1').currentTime(39);
     }
 });
 
@@ -269,10 +316,10 @@ videojs('video3').on('ended', () => {
     div1.classList.remove("hidden");
     video3played = true
     if (video2played && video3played && video4played) {
-        videojs('video1').currentTime(45);
+        videojs('video1').currentTime(42);
         videojs('video1').play();
     } else {
-        videojs('video1').currentTime(43);
+        videojs('video1').currentTime(39);
     }
 });
 
@@ -281,14 +328,9 @@ videojs('video4').on('ended', () => {
     div1.classList.remove("hidden");
     video4played = true
     if (video2played && video3played && video4played) {
-        videojs('video1').currentTime(45);
+        videojs('video1').currentTime(42);
         videojs('video1').play();
     } else {
-        videojs('video1').currentTime(43);
+        videojs('video1').currentTime(39);
     }
 });
-
-
-
-
-
